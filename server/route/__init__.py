e3e4b14567987f9e74df68c88c1c337ef468e00b
feiticeiro-tec <silviohenriquecruzdasilva@gmail.com>
2022-05-30 09:09:39 -0300

    split route -> json and csv

--- server/route/__init__.py
+++ server/route/__init__.py
@@ -11,9 +11,41 @@ logging.info('Start Routes')
 app = current_app
 
 
-@app.route('/<file>/')
-async def index(file):
+@app.route('/json/')
+async def index():
 
+    index = request.args.get('index')
+
+    try:  # get args year and month
+        start_month = capture_month(request, 'start_month')
+        end_month = capture_month(request, 'end_month')
+        start_year = capture_year(request, 'start_year')
+        end_year = capture_year(request, 'end_year')
+    except DateInputError as error:
+        return ResponseErrors(str(error), request).value, 400
+
+    data_index = {}
+    if index:  # filter index
+        # filter index in range date values
+        items = await Index.filter(name=index, date__range=[f'{start_year}-{start_month}', f'{end_year}-{end_month}']).order_by('date')
+        for item in items:
+            data_index[item.get_date_format] = item.get_index_format
+        return jsonify(data_index)
+
+    else:
+        # filter index in range date values
+        items = await Index.filter(date__range=[f'{start_year}-{start_month}', f'{end_year}-{end_month}']).order_by('date')
+
+        for item in items:
+            if data_index.get(item.name):
+                data_index[item.name][item.get_date_format] = item.get_index_format
+            else:
+                data_index[item.name] = {item.get_date_format: item.get_index_format}
+
+        return jsonify(data_index)
+
+@app.route('/csv/')
+async def route_csv():
     index = request.args.get('index')
 
     try:  # get args year and month
@@ -30,32 +62,13 @@ async def index(file):
         # filter index in range date values
         items = await Index.filter(name=index, date__range=[f'{start_year}-{start_month}', f'{end_year}-{end_month}']).order_by('date')
 
-        if file == 'csv':
-            file_temp = tempfile.NamedTemporaryFile()
-            
-            data_index = [{'date': item.get_date_format, 'index': item.get_index_format} for item in items]
+        file_temp = tempfile.NamedTemporaryFile()
+        
+        data_index = [{'date': item.get_date_format, 'index': item.get_index_format} for item in items]
 
-            df = pandas.DataFrame(data_index)
-            df.to_csv(file_temp.name, encoding='utf-8', index=False, sep=';')
-            return send_file(file_temp.name, download_name=f'{index}_{start_month}-{start_year}_{end_month}-{end_year}.csv')
-
-        else:
-            data_index = {}
-            for item in items:
-                data_index[item.get_date_format] = item.get_index_format
-            return jsonify(data_index)
+        df = pandas.DataFrame(data_index)
+        df.to_csv(file_temp.name, encoding='utf-8', index=False, sep=';')
+        return send_file(file_temp.name, download_name=f'{index}_{start_month}-{start_year}_{end_month}-{end_year}.csv')
 
     else:
-
-        data_index = {}
-
-        # filter index in range date values
-        items = await Index.filter(date__range=[f'{start_year}-{start_month}', f'{end_year}-{end_month}']).order_by('date')
-
-        for item in items:
-            if data_index.get(item.name):
-                data_index[item.name][item.get_date_format] = item.get_index_format
-            else:
-                data_index[item.name] = {item.get_date_format: item.get_index_format}
-
-        return jsonify(data_index)
+        return ResponseErrors('index Not Found',request).value, 400   
